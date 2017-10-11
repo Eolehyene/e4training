ccd24d82a03533ebbfff51952c7ae64ace620ca3
formation <formation@formation-Aspire-X3995>
2017-10-11 16:08:15 +0200

    Create logicals nodes

--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalProvider.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalProvider.java
@@ -7,7 +7,9 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
 import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
 
 public class RentalProvider extends LabelProvider implements ITreeContentProvider {
 
@@ -26,7 +28,11 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		if (parentElement instanceof RentalAgency) {
-			return ((RentalAgency) parentElement).getCustomers().toArray();
+			RentalAgency a =  (RentalAgency) parentElement;
+			return new Node[] {new Node(Node.CUSTOMERS, a), new Node(Node.RENTAL, a), new Node(Node.RENTAL_OBJECT, a)};
+		}
+		else if (parentElement instanceof Node) {
+			return ((Node) parentElement).getChildren();
 		}
 		return null;
 	}
@@ -59,8 +65,49 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		if (element instanceof Customer) {
 			return ((Customer) element).getFirstName() + " " + ((Customer) element).getLastName();
 		}
+		if (element instanceof Rental) {
+			return ((Rental)element).toString();
+		}
+		if (element instanceof RentalObject) {
+			return ((RentalObject) element).getName();
+		}
+			
 		return super.getText(element);
 	}
 	
 
+}
+
+class Node {
+	static final String RENTAL_OBJECT = "Rental Object";
+	static final String RENTAL = "Rental";
+	static final String CUSTOMERS = "Customers";
+	private String label;
+	RentalAgency ra;
+	public Node(String label, RentalAgency ra) {
+		super();
+		this.label = label;
+		this.ra = ra;
+	}
+	
+	public Object[] getChildren()
+	{
+		if (label == CUSTOMERS)
+		{
+			return ra.getCustomers().toArray();
+		}
+		else if (label == RENTAL) {
+			return ra.getRentals().toArray();
+		}
+		else if (label == RENTAL_OBJECT) {
+			return ra.getObjectsToRent().toArray();
+		}
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return label;
+	}
 }
