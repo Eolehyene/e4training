75da2199b016e5467d2ffa335fd8202c2a1fdfc2
formation <formation@formation-Aspire-X3995>
2017-10-13 10:09:34 +0200

    Iteration on extensions points

--- com.sii.rental.ui/src/com/sii/rental/ui/RentalAddon.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/RentalAddon.java
@@ -3,6 +3,9 @@ package com.sii.rental.ui;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Optional;
@@ -19,7 +22,7 @@ import com.opcoach.training.rental.core.helpers.RentalAgencyGenerator;
 
 public class RentalAddon implements RentalUIConstants{
 	@PostConstruct
-	public void init (IEclipseContext ctx) {
+	public void init (IEclipseContext ctx, IExtensionRegistry reg) {
 		ctx.set(RentalAgency.class, RentalAgencyGenerator.createSampleAgency());
 		ctx.set(RENTAL_UI_IMG_REGISTRY, getLocalImageRegistry());
 		ctx.set(RENTAL_UI_PREF_STORE, new ScopedPreferenceStore(InstanceScope.INSTANCE, PLUGIN_ID));
@@ -42,11 +45,23 @@ public class RentalAddon implements RentalUIConstants{
 		reg.put(IMG_EXPAND_ALL, ImageDescriptor.createFromURL(b.getEntry(IMG_EXPAND_ALL)));
 
 		return reg;
-}
+	}
 	
 	@Inject
 	@Optional
 	void reactOnRentalEvent(@UIEventTopic("rental/copy") Customer c) {
 		System.out.println("Customer " + c.getDisplayName() + " copied");
+	}
+	
+	@Inject
+	public void getExtensions(IExtensionRegistry reg) {
+		for (IConfigurationElement elt : reg.getConfigurationElementsFor("org.eclipse.e4.workbench.model")) {
+			if (elt.getName().compareTo("fragment") == 0) {
+				System.out.println(elt.getNamespaceIdentifier() + " : Found  " +  elt.getName() + " with uri: " + elt.getAttribute("uri"));
+			}
+			else if (elt.getName().compareTo("fragment") == 0) {
+				System.out.println(elt.getNamespaceIdentifier() + " : Found " +  elt.getName() + " with uri: " + elt.getAttribute("class"));
+			}
+		} 
 	}
 }
